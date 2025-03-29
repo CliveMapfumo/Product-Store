1e14fd091d4f4830d86d071783c726ed5b38a62e
Clive Mapfumo <mapfumoanderson@gmail.com>
2025-03-30 01:14:13 +0200

    managed to add the get all products, delete and add api

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,21 @@ const app = express();
 
 app.use(express.json()) //allows us to accept json data in the req.body
 
+app.get("/api/products", async (req,res)=>{
+    try {
+        const products = await Product.find({})
+        res.status(200).json({sucess:true, data: products})
+    } catch (error) {
+        console.log("Error in fetching Products", error.message)
+        res.status(500).json({sucess: false, message: "Server Error"});
+        
+    }
+})
+
+
+
+
+
 app.post("/api/products", async (req, res)=>{
     const product = req.body; // user sends this data
     if(!product.name || !product.price ||!product.image ){
@@ -23,6 +38,44 @@ app.post("/api/products", async (req, res)=>{
         
     }
 })
+
+app.put("/api/products/:id", async (req,res)=>{
+    const {id} = req.params
+    const product = req.body
+
+
+
+    try {
+        const updatedProduct= await Product.findByIdAndUpdate(id, product, {new:true})
+        res.status(200).json({sucess:true, data: updatedProduct})
+
+    } catch (error) {
+        res.status(500).json({sucess: false, message: "Server Error"});
+    }
+
+
+})
+
+
+
+
+
+app.delete("/api/products/:id", async(req,res)=>{
+    const {id} = req.params
+    try {
+        await Product.findByIdAndDelete(id)
+        res.status(200).json({sucess:true, message:"Product deleted"})
+        
+    } catch (error) {
+        res.status(404).json({sucess:false, message:"Product not found"})
+   
+    }
+})
+
+
+
+
+
 
 console.log(process.env.MONGO_URI)
 
