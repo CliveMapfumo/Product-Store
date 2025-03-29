ef9dd082f6240e25543c2f3d2c548b2a509481dc
Clive Mapfumo <mapfumoanderson@gmail.com>
2025-03-29 22:19:33 +0200

    managed to crate products working server

--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,22 @@ dotenv.config();
 
 const app = express();
 
-app.post("/products", async (req, res)=>{
+app.use(express.json()) //allows us to accept json data in the req.body
+
+app.post("/api/products", async (req, res)=>{
     const product = req.body; // user sends this data
-    if(!product.name || !product.price ||product.image ){
+    if(!product.name || !product.price ||!product.image ){
         return res.status(400).json({sucess: false, message: "Please provide all fields"});
     }
     const newProduct = new Product(product)
-
+    try {
+        await newProduct.save();
+        res.status(200).json({sucess: true, data: newProduct});
+    } catch (error) {
+        console.error("Error in Create Product", error.message)
+        res.status(500).json({sucess: false, message: "Server Error"});
+        
+    }
 })
 
 console.log(process.env.MONGO_URI)
